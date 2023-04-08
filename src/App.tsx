88c1c059164d948eaa7764d5e8f019716c69f993
Jordan Orozco <joaorozc@bancolombia.com.co>
2023-04-08 10:20:16 -0500

    Nuevos paquetes y configuración de rutas

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+
+
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+
+import HomeView from './pages/Home';
+
 import './App.css';
 
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route>
+			<Route path="/" element={<HomeView />} />
+		</Route>
+	)
+);
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<RouterProvider router={router} />
+	)
 }
 
 export default App;
